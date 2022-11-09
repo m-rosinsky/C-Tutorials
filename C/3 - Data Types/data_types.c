079d484287a0836ff2feda21560cf7f1eaaa20ab
Mike Rosinsky <56515138+m-rosinsky@users.noreply.github.com>
2022-11-09 10:24:46 -0500

    Create data_types.c

---
+++ C/3 - Data Types/data_types.c
@@ -0,0 +1,21 @@
+int main()
+{
+    // Delcare an integer named x and assign it the value 5.
+    int x = 5;
+  
+    // Declare a double y and assign it the value 0.0.
+    double y = 0.0;
+  
+    /*
+    Data does not necessarily need to have an initial value.
+    This syntax is perfectly fine.
+    */
+    int z; // z's initial value is undefined.
+  
+    // Set z to -3.
+    z = -3;
+  
+    // Create a character to hold the letter 'A'.
+    char my_char = 'A';
+    
+}
