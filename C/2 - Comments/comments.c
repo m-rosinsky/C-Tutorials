499c897122543bdbcc614ccd40e8fb678671fc3c
Mike Rosinsky <56515138+m-rosinsky@users.noreply.github.com>
2022-11-09 09:41:20 -0500

    Create comments.c

---
+++ C/2 - Comments/comments.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+This function is the main function of our program.
+It's where program execution starts!
+*/
+int main()
+{
+    // Print to the console!
+    printf("Hello, World!");
+}
