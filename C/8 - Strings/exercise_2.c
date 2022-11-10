ddfb5115472d89e705a7ba22b364f769295d31d9
Mike Rosinsky <56515138+m-rosinsky@users.noreply.github.com>
2022-11-10 11:26:51 -0500

    Create exercise_2.c

---
+++ C/8 - Strings/exercise_2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+/*
+TODO: Dynamically allocate enough space
+to store a copy of s on the heap.
+Then copy the contents of s into the new
+string and return it.
+*/
+char * alloc_string(char * s)
+{
+
+}
+
+// Do not modify code below this point.
+#define NUM_INPUTS 4
+int main()
+{
+    char* inputs[NUM_INPUTS] = {"hello, world", "", "the quick brown fox jumped over the lazy dog", "a%hs 2h"};
+    
+    for (size_t i = 0; i < NUM_INPUTS; i++)
+    {
+        char * new_s = alloc_string(inputs[i]);
+        if (0 == strcmp(new_s, inputs[i]))
+        {
+            printf("Test Case %lu Passed!\n", i);
+        }
+        else
+        {
+            printf("Test Case %lu Failed: (%s)\n", i, inputs[i]);
+        }
+    }
+}
