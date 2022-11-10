45348c989b6c09e7cbce71e21875e2f672f56f29
Mike Rosinsky <56515138+m-rosinsky@users.noreply.github.com>
2022-11-10 11:31:46 -0500

    Update exercise_2.c

--- C/8 - Strings/exercise_2.c
+++ C/8 - Strings/exercise_2.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 /*
 TODO: Dynamically allocate enough space
 to store a copy of s on the heap.
+
 Then copy the contents of s into the new
 string and return it.
+
+Don't forget to allocate space for the
+null terminator.
 */
 char * alloc_string(char * s)
 {
-
+    // TODO: Write code here.
 }
 
 // Do not modify code below this point.
@@ -17,7 +22,7 @@ char * alloc_string(char * s)
 int main()
 {
     char* inputs[NUM_INPUTS] = {"hello, world", "", "the quick brown fox jumped over the lazy dog", "a%hs 2h"};
-    
+
     for (size_t i = 0; i < NUM_INPUTS; i++)
     {
         char * new_s = alloc_string(inputs[i]);
@@ -29,5 +34,6 @@ int main()
         {
             printf("Test Case %lu Failed: (%s)\n", i, inputs[i]);
         }
+        free(new_s);
     }
 }
