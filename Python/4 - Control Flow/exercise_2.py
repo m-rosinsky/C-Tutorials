62ba74de3036e5848df0a7ef1483fd8de5246be5
Mike Rosinsky <56515138+m-rosinsky@users.noreply.github.com>
2022-11-16 09:51:28 -0500

    Create exercise_2.py

---
+++ Python/4 - Control Flow/exercise_2.py
@@ -0,0 +1,48 @@
+"""
+This function should loop through a string
+and print only the consonants (including y).
+
+Example:
+input   -> "apple"
+output  -> "ppl"
+
+input   -> "bendy"
+output  -> "bndy"
+
+input   -> "aeiou"
+output  -> ""
+"""
+def print_consonants(s):
+    # TODO: WRITE CODE HERE
+    
+# DO NOT MODIFY CODE BELOW THIS POINT
+def main():
+    """
+This function should take in one integer.
+
+If the integer is negative, print "Negative"
+If the integer is zero, print "Zero"
+If the integer is positive, print "Positive"
+"""
+def eval_int(n):
+    # TODO: WRITE CODE HERE
+  
+# DO NOT MODIFY CODE BELOW THIS POINT
+def main():
+    print("Test 1:")
+    print("Expected Output:\nppl")
+    print("Your Output:")
+    print_consonants("apple")
+    
+    print("\nTest 2:")
+    print("Expected Output:\nth qck brwn fx jmps vr th lzy dg")
+    print("Your Output:")
+    print_consonants("the quick brown fox jumps over the lazy dog")
+    
+    print("\nTest 3:")
+    print("Expected Output:\n")
+    print("Your Output:")
+    eval_int("aueiaoiuaeioue")
+  
+if __name__=='__main__':
+    main()
